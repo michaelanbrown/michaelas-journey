9d15eff4517f61a8e4b42132fee693ce4052b14e
Michaela Brown <m_brownbrown@yahoo.com>
2022-10-10 18:49:24 -0700

    changing background color, adding small intro paragraph and photo

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const h2 = document.createElement("h2");
-h2.textContent = "October 4th, 2022 is when I finally understood how to live my life.";
+h2.textContent = "Hello, my name is Michaela.";
 
 document.querySelector("body").appendChild(h2)
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a 23 year old with a past that, personally, I believe isn't so bright.";
+
+document.querySelector("body").appendChild(h3)
+
+const h4 = document.createElement("h4");
+h4.textContent = "October 4th, 2022 is when I finally understood how to live my life.";
+
+document.querySelector("body").appendChild(h4)
