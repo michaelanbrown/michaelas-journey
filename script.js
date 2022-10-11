324da3e5592cb87a52390ed23fda2bc05e5d7c02
Michaela Brown <m_brownbrown@yahoo.com>
2022-10-10 18:52:06 -0700

    minor edits

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Hello, my name is Michaela.";
+// const h2 = document.createElement("h2");
+// h2.textContent = "Hello, my name is Michaela.";
 
-document.querySelector("body").appendChild(h2)
+// document.querySelector("body").appendChild(h2)
 
-const h3 = document.createElement("h3");
-h3.textContent = "I'm a 23 year old with a past that, personally, I believe isn't so bright.";
+// const h3 = document.createElement("h3");
+// h3.textContent = "I'm a 23 year old with a past that, personally, I believe isn't so bright.";
 
-document.querySelector("body").appendChild(h3)
+// document.querySelector("body").appendChild(h3)
 
-const h4 = document.createElement("h4");
-h4.textContent = "October 4th, 2022 is when I finally understood how to live my life.";
+// const h4 = document.createElement("h4");
+// h4.textContent = "October 4th, 2022 is when I finally understood how to live my life.";
 
-document.querySelector("body").appendChild(h4)
+// document.querySelector("body").appendChild(h4)
